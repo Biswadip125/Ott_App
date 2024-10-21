75772b1b18bf6f18a160be49f84b7fe6b4c6c620
Biswadip125 <162881134+Biswadip125@users.noreply.github.com>
2024-10-21 13:10:17 +0530

    Update TvShowsContainer.jsx

--- Frontend/Mern App Frontend/src/components/TvShowsContainer.jsx
+++ Frontend/Mern App Frontend/src/components/TvShowsContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import MovieList from "./Movielist";
+import MovieList from "./MovielList";
 
 const TvShowsContainer = () => {
   const movie = useSelector((store) => store.movie);
