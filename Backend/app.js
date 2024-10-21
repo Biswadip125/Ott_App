b50fe93d20aa51f8e03e8b5e0a5a8ccd3290728a
Biswadip125 <162881134+Biswadip125@users.noreply.github.com>
2024-10-21 17:03:33 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X_Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+    "Origin, X_Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization"
   );
   next();
 });
