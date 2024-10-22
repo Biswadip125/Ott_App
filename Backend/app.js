f29240452ec145a319adc862848d0e83b92be754
Biswadip125 <162881134+Biswadip125@users.noreply.github.com>
2024-10-22 17:12:57 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "https://ott-kiz45x4s7-biswadip125s-projects.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "https://ott-app-hazel.vercel.app");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, PUT, POST, DELETE, UPDATE, OPTIONS "
